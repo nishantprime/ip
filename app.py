e84242e8233fb5b1ff9dec6177f533ad2ab9afde
nishantprime <nishantkumarprime@gmail.com>
2024-12-09 20:49:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask('__name__')
+
+@app.route('/'):
+def print_ip():
+    ip_route = request.headers.get("X-Forwarded-For")
+    user_ip = ip_route.split(',')[0].strip()
+    return f"Your IP Addresss is {user_ip}"
+
+if '__name__' == '__main__':
+    app.run()
