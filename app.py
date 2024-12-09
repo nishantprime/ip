6e8a371820ec1d73a50f0e7253245df731f6bac9
nishantprime <nishantkumarprime@gmail.com>
2024-12-09 20:57:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 @app.route('/')
 def print_ip():
     ip_route = request.headers.get("X-Forwarded-For")
     user_ip = ip_route.split(',')[0].strip()
-    return f"Your IP Addresss is {user_ip}"
+    return f"Your IP Addresss is {user_ip} \n\nAnd IP Route is {ip_route}"
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     app.run()
